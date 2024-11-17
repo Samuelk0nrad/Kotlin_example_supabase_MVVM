aef36510ea1345eb2d711997939d6078aa69621b
Samuelk0nrad <114510161+Samuelk0nrad@users.noreply.github.com>
2024-11-17 16:47:21 +0100

    if user is not authenticated / Refresh token is not correct go to Auth screen

--- composeApp/src/commonMain/kotlin/org/example/project/AppViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/AppViewModel.kt
@@ -1,7 +1,11 @@
 package org.example.project
 
 import androidx.lifecycle.ViewModel
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.auth.auth
 
-class AppViewModel : ViewModel() {
-
+class AppViewModel(
+    supabaseClient: SupabaseClient
+) : ViewModel() {
+    val sessionStatus = supabaseClient.auth.sessionStatus
 }
--- composeApp/src/commonMain/kotlin/org/example/project/Navigation.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/Navigation.kt
@@ -1,9 +1,13 @@
 package org.example.project
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import io.github.jan.supabase.auth.status.SessionStatus
 import org.example.project.authentication.Authentication
 import org.example.project.authentication.AuthenticationViewModel
 import org.example.project.utils.Screens
@@ -17,8 +21,11 @@ fun Navigation(){
     KoinContext {
         val appViewModel = koinViewModel<AppViewModel>()
 
+        // Log In State
+        val sessionStatus by appViewModel.sessionStatus.collectAsState()
+
         val navController = rememberNavController()
-        NavHost(navController = navController, startDestination = Screens.Authentication.title) {
+        NavHost(navController = navController, startDestination = Screens.Page2.title) {
 
             composable(Screens.Authentication.title) {
 
@@ -31,7 +38,21 @@ fun Navigation(){
             composable(Screens.Page2.title) {
 
             }
+        }
 
+        // Check if the user is authenticated if not navigate to the authentication screen
+        LaunchedEffect(sessionStatus) {
+            when (sessionStatus) {
+                is SessionStatus.NotAuthenticated -> {
+                    navController.navigate(Screens.Authentication.title)
+                }
+                is SessionStatus.RefreshFailure -> {
+                    navController.navigate(Screens.Authentication.title)
+                }
+                else -> {
+                    // Do nothing
+                }
+            }
         }
     }
 }
